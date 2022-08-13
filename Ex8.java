964c128166735b77c42c491ea0150c092361b595
AfrahObaid <111083196+AfrahObaid@users.noreply.github.com>
2022-08-13 14:29:09 +0300

    Add files via upload

---
+++ Ex8.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Ex8 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello,Afrah");
+    }
+}
+
---
+++ Ex9.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        String name= JOptionPane.showInputDialog("Afrah");
+        System.out.print("Hello ");
+        System.out.print(name);
+    }
+}
